ff9ef17b759b35effea94065b39bb4e202d36612
tomtomy8 <tommy.birt@gmail.com>
2013-07-26 10:51:33 +0100

    First commit

---
+++ BuzBugViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuzBugViewController.h
+//  BuzBug
+//
+//  Created by The Birts on 26/07/2013.
+//  Copyright (c) 2013 Silverstorm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuzBugViewController : UIViewController
+
+@end
---
+++ BuzBugViewController.m
@@ -0,0 +1,29 @@
+//
+//  BuzBugViewController.m
+//  BuzBug
+//
+//  Created by The Birts on 26/07/2013.
+//  Copyright (c) 2013 Silverstorm. All rights reserved.
+//
+
+#import "BuzBugViewController.h"
+
+@interface BuzBugViewController ()
+
+@end
+
+@implementation BuzBugViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
